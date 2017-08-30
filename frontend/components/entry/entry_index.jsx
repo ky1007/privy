742336c4b8af6a7cde39d3d8dc1f16538cd18bd3
ky1007 <kevinXyang@outlook.com>
2017-08-30 11:56:51 -0700

    Remove emoji error

--- frontend/components/entry/entry_index.jsx
+++ frontend/components/entry/entry_index.jsx
@@ -37,7 +37,7 @@ class EntryIndex extends Component {
     // }
     return (
       <span>
-        people you follow <span role="img" aria-label="Pair dancing emoji">👯‍♂️</span>
+        people you follow <span role="img" aria-label="Pair dancing emoji">👯‍♂</span>
       </span>
     );
   }
