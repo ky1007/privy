cf327a739a1f8e19e54b4608867ffc9a638d82f8
ky1007 <kevinXyang@outlook.com>
2017-10-25 00:59:26 -0700

    Update production README to include more GIFs of Privy in action

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # Privy 
-[Privy][live-link] is a single page application that lets users publish an anonymous diary under a pseudonym.  The app has a full-stack implementation: frontend views are handled with React and Redux. The backend utilizes a Rails framework to render and send data from a Postgres database to the frontend.
+[Privy][live-link] is a single page, full-stack application that lets users publish an anonymous diary under a pseudonym. The application is implemented via an MVC architecture where Ruby on Rails acts as the Model and Controller and React is responsible for rendering the Views. The React frontend is also paired with a Redux flux architecture to manage state and ensure unidirectional data flow.
+
+Privy is passion project born out of my own curiosity about how other people think and experience life, and out of a desire publish my own experiences for others to read.
 
 
 ## Features
-  * Secure account login authentication and creation: passwords are hashed and salted via BCrypt on the backend, additional protection reinforced on the frontend via protected auth routes
-  * Users can follow and read other people’s diaries
-  * Users can save and bookmarks diary entries they find meaningful
-  * Users can read through their old entries and post a reflection entry they’ve written. Other users can read an entry’s reflections, but they cannot write a reflection on entries they haven’t written. 
+  * Secure account login authentication and creation: passwords are hashed and salted via BCrypt on the backend, additional reinforced protection is provided on the frontend via protected routes in React.
+  * Users can follow and read other people’s published diaries.
+  * Users can save and bookmark diary entries to reference to later.
+  * Users can post a reflection on an old entry they've written. (Other users can read an entry’s reflections, but they cannot write a reflection on entries they haven’t written.)
+
+## Previews of Privy in action
+### Browsing Entries
+![Browsing Entries](docs/images/privy-browse.gif)
+
+### Following Users
+![Following Users](docs/images/privy-follow.gif)
+
+### Writing a personal reflection
+![Writing a personal reflection](docs/images/privy-reflection.gif)
+
+### Deleting and Editing Diary Entries
+![Editing and deleting diary entries](docs/images/privy-entry-options.gif)
 
  ### Login and Greeting page
   ![Privy login and greeting page](docs/images/privy-previews.png)
@@ -50,14 +65,9 @@ Utilizing SASS and its ability to create mixins also helped maintain DRY code fo
 Privy was designed and built in a two week period. View the original [proposal][dev-readme], which includes MVP features, an implementation timeline, and more extensive documentation.
 
 ## Future Implementations
-### Feature implementations
-  * User profiles to show general demographic information about the writers
   * Ability to search and read diaries from a general demographic, e.g. Italian women in their 40’s living in South Africa
-  * The ability to highlight and save snippets of text users related to
-
-### Technical Implementations
+  * The ability to highlight and save snippets of text users resonate with and like.
   * Adding unit, integration, and end-to-end tests
-  * Utilize more CSS animations to create a smoother user experience
 
-  [live-link]: https://privy-journal.herokuapp.com
+  [live-link]: http://wwww.privy.site/
   [dev-readme]: docs/README.md
--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -20,33 +20,101 @@ const loggedOutGreeting = () => (
             <AuthRoute path="/" component={SessionFormContainer} />
           </div>
         </div>
-        <section className="welcome-container">
-          <section className="feature">
-            <h2>Read</h2>
-            <h3>what it's like to be someone else.</h3>
-            Discover how other people experience and approach their life. 
-          </section>
-          <section className="feature">
-            <h2>Write</h2>
-            <h3>and reflect.</h3>
-            Take time to figure out what's been happening in your life and where you want to go.
-          </section>
-          <section className="feature">
-            <h2>Follow</h2>
-            <h3>interesting people.</h3>
-            Keep up with those who inspire you and keep you curious.
-          </section>
-          <section className="feature">
-            <h2>Bookmark</h2>
-            <h3>what speaks to you.</h3>
-            Keep and hold the things you've read that resonates with you.
-          </section>
+        <section className="feature-section">
+          <div className="feature-container">
+
+            <div className="row">
+              <div className="column column-4">
+                <img src="https://i.imgur.com/higmKvN.png" />
+                {/* Quisque vel mollis sem. Fusce nunc ante, vulputate id scelerisque sed, pretium vitae leo. Ut pellentesque, eros a venenatis pretium, urna mi commodo mauris, et fermentum dolor nunc eu orci. Mauris efficitur vel orci non fermentum. Pellentesque consectetur, dolor vel ullamcorper scelerisque, est leo suscipit orci, et ultrices eros nisl a sem. Nunc augue libero, feugiat a odio vitae, iaculis rhoncus libero. Donec eros augue, suscipit nec condimentum eget, elementum in lorem. Proin tempus ornare magna quis mattis. Curabitur at interdum magna, at tincidunt lacus. Integer porttitor vitae libero ac auctor. Duis in enim fermentum, pretium mauris at, porta sapien. Praesent elementum sem massa, sit amet placerat diam posuere gravida. Integer vel scelerisque lorem. Proin lobortis tortor non leo faucibus, ac tincidunt orci molestie. */}
+              </div>
+              <div className="column column-8 feature">
+                <h2>Read</h2>
+                <h3>what it's like to be someone else.</h3>
+                Discover how other people experience and approach their life from a candid, anonymous perspective. 
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="column column-4">
+                <img src="https://i.imgur.com/higmKvN.png" />
+                {/* Quisque vel mollis sem. Fusce nunc ante, vulputate id scelerisque sed, pretium vitae leo. Ut pellentesque, eros a venenatis pretium, urna mi commodo mauris, et fermentum dolor nunc eu orci. Mauris efficitur vel orci non fermentum. Pellentesque consectetur, dolor vel ullamcorper scelerisque, est leo suscipit orci, et ultrices eros nisl a sem. Nunc augue libero, feugiat a odio vitae, iaculis rhoncus libero. Donec eros augue, suscipit nec condimentum eget, elementum in lorem. Proin tempus ornare magna quis mattis. Curabitur at interdum magna, at tincidunt lacus. Integer porttitor vitae libero ac auctor. Duis in enim fermentum, pretium mauris at, porta sapien. Praesent elementum sem massa, sit amet placerat diam posuere gravida. Integer vel scelerisque lorem. Proin lobortis tortor non leo faucibus, ac tincidunt orci molestie. */}
+              </div>
+              <div className="column column-8 feature">
+                <h2>Write</h2>
+                <h3>and reflect.</h3>
+                Take time to figure out what's been happening in your life, where you want to go, and who you want to be.
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="column column-4">
+                <img src="https://i.imgur.com/higmKvN.png" />
+                {/* Quisque vel mollis sem. Fusce nunc ante, vulputate id scelerisque sed, pretium vitae leo. Ut pellentesque, eros a venenatis pretium, urna mi commodo mauris, et fermentum dolor nunc eu orci. Mauris efficitur vel orci non fermentum. Pellentesque consectetur, dolor vel ullamcorper scelerisque, est leo suscipit orci, et ultrices eros nisl a sem. Nunc augue libero, feugiat a odio vitae, iaculis rhoncus libero. Donec eros augue, suscipit nec condimentum eget, elementum in lorem. Proin tempus ornare magna quis mattis. Curabitur at interdum magna, at tincidunt lacus. Integer porttitor vitae libero ac auctor. Duis in enim fermentum, pretium mauris at, porta sapien. Praesent elementum sem massa, sit amet placerat diam posuere gravida. Integer vel scelerisque lorem. Proin lobortis tortor non leo faucibus, ac tincidunt orci molestie. */}
+              </div>
+              <div className="column column-8 feature">
+                <h2>Follow</h2>
+                <h3>interesting people.</h3>
+                Keep up with those who inspire you and keep you curious.
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="column column-4">
+                <img src="https://i.imgur.com/higmKvN.png" />
+                {/* Quisque vel mollis sem. Fusce nunc ante, vulputate id scelerisque sed, pretium vitae leo. Ut pellentesque, eros a venenatis pretium, urna mi commodo mauris, et fermentum dolor nunc eu orci. Mauris efficitur vel orci non fermentum. Pellentesque consectetur, dolor vel ullamcorper scelerisque, est leo suscipit orci, et ultrices eros nisl a sem. Nunc augue libero, feugiat a odio vitae, iaculis rhoncus libero. Donec eros augue, suscipit nec condimentum eget, elementum in lorem. Proin tempus ornare magna quis mattis. Curabitur at interdum magna, at tincidunt lacus. Integer porttitor vitae libero ac auctor. Duis in enim fermentum, pretium mauris at, porta sapien. Praesent elementum sem massa, sit amet placerat diam posuere gravida. Integer vel scelerisque lorem. Proin lobortis tortor non leo faucibus, ac tincidunt orci molestie. */}
+              </div>
+              <div className="column column-8 feature">
+                <h2>Bookmark</h2>
+                <h3>what speaks to you.</h3>
+                Keep and hold the things you've read that resonates with you.
+              </div>
+            </div>
+
+            {/* <section className="feature">
+              <div className="feature-image">
+                <h2>photo goes here</h2>
+              </div>
+              <div className="feature-description">
+                <h2>Read</h2>
+                <h3>what it's like to be someone else.</h3>
+                Discover how other people experience and approach their life from a candid, anonymous perspective. 
+              </div>
+            </section>
+            <section className="feature">
+              <div className="feature-image">
+                <h2>photo goes here</h2>
+              </div>
+              <div className="feature-description">
+                <h2>Write</h2>
+                <h3>and reflect.</h3>
+                Take time to figure out what's been happening in your life, where you want to go, and who you want to be.
+              </div>
+            </section>
+            <section className="feature">
+              <div className="feature-image">
+                <h2>photo goes here</h2>
+              </div>
+              <div className="feature-description">
+                <h2>Follow</h2>
+                <h3>interesting people.</h3>
+                Keep up with those who inspire you and keep you curious.
+              </div>
+            </section>
+            <section className="feature">
+              <div className="feature-image">
+                <h2>photo goes here</h2>
+              </div>
+              <div className="feature-description">
+                <h2>Bookmark</h2>
+                <h3>what speaks to you.</h3>
+                Keep and hold the things you've read that resonates with you.
+                </div>
+            </section> */}
+          </div>
         </section>
       </div>
     </div>
-    {/* <div className="main-container">
-      Heyhey
-    </div> */}
   </div>
 );
 
