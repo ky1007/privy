05c8fb1a1c3336e56e8f035540800d1ae5c975aa
ky1007 <kevinXyang@outlook.com>
2018-02-19 23:37:09 -0800

    Other nav menu stylings

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -46,17 +46,20 @@ class LoggedInLinks extends React.Component {
       <nav className="main-container">
         <header className="nav-logo"><Link to="/welcome">Privy</Link></header>
         <section className={this.state.showNav ? 'nav-links' : 'invisible' }>
-        <li><NavLink to="/everyone" activeClassName="nav-active-link">Everyone&apos;s Diary</NavLink></li>
+          {/* <div className="pointer"></div> */}
+          <li><NavLink to="/everyone" activeClassName="nav-active-link">Everyone&apos;s Diary</NavLink></li>
           <li><NavLink to={`/${this.props.currentUser.username}/entries`} activeClassName="nav-active-link">Your Diary</NavLink></li>
           <li><NavLink to="/feed" activeClassName="nav-active-link">Your Feed</NavLink></li>
           <li><NavLink to="/bookmarks" activeClassName="nav-active-link">Your Bookmarks</NavLink></li>
           <li><NavLink to="/new_entry" activeClassName="nav-active-link">New Entry</NavLink></li>
-          <li><NavLink to="/" onClick={this.props.logout} className="logout-button">
+          {/* <li><NavLink to="/" onClick={this.props.logout} className="logout-button"> */}
+          <li><NavLink to="/" onClick={this.props.logout}>
                 Logout, {this.props.currentUser.username}
               </NavLink>
           </li>
         </section>
-        <li onClick={this.menuVisibilityToggle}><i className="fa fa-bars fa-2x" /></li>
+        <li onClick={this.menuVisibilityToggle}><i className={this.state.showNav ? 'fa fa-bars fa-2x lighten' : 'fa fa-bars fa-2x'} /></li>
+        {/* <li onClick={this.menuVisibilityToggle}><i className="fa fa-bars fa-2x" /></li> */}
         {/* <li onClick={this.menuVisibilityToggle}><i className="fa fa-user-circle fa-2x" /></li> */}
         {/* <NavDropdown display={`${toggle} nav-pop`} logout={logout} currentUser={currentUser}/> */}
       </nav>
